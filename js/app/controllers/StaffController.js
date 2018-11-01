ff68fb7bfa140475e3deede450e1003cdb207fc8
Jim Rectenwald <jimrec101@gmail.com>
2018-10-31 23:19:21 -0400

    done

--- js/app/controllers/StaffController.js
+++ js/app/controllers/StaffController.js
@@ -1,8 +1,7 @@
-function StaffController($scope) {
-    $scope.name = 'Steve Jobs';
-    $scope.email = 'steve@apple.com';
+function StaffController() {
+    this.name = 'Steve Jobs';
+    this.email = 'steve@apple.com';
 }
-
-angular
+ angular
     .module('app')
     .controller('StaffController', StaffController);
